f3f1c5f652c431ba8040c5bf729785d010c39e2b
AntoDev <antolin9861@hotmail.com>
2025-05-23 17:02:49 +0200

    Help

--- eb/help.js
+++ eb/help.js
@@ -2,6 +2,7 @@ import { EmbedBuilder } from 'discord.js';
 
 const embed = new EmbedBuilder()
     .setColor(3066993)
+    .setTitle("Help")
     .setDescription("Ayuda.\nComandos:\n/eb-m1, envia un Embed Construido\n/embed, envia un Embed que lo haces Tu")
     .setAuthor({
         name: "Bot Bloody.",
