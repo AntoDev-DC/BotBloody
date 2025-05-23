ef9803d11ed07a438fefe323f53629038c197054
AntoDev <antolin9861@hotmail.com>
2025-05-23 15:50:25 +0200

    Update Deploy Commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,28 +1,34 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+
+console.log('Token:', process.env.DISCORD_TOKEN ? 'CARGADO' : 'NO CARGADO');
+console.log('ClientID:', process.env.CLIENT_ID || 'NO CARGADO');
 
 const commands = [];
-const cmdPath = path.join(__dirname, 'cmd');
-const commandFiles = fs.readdirSync(cmdPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'cmd');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-  const command = require(`./cmd/${file}`);
-  commands.push(command.data.toJSON());
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    commands.push(command.data.toJSON());
+  }
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
   try {
-    console.log('⏳ Registrando comandos...');
+    console.log(`Registrando ${commands.length} comandos globalmente...`);
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-      { body: commands }
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands },
     );
-    console.log('✅ Comandos registrados con éxito');
-  } catch (err) {
-    console.error(err);
+    console.log('¡Comandos registrados globalmente!');
+  } catch (error) {
+    console.error(error);
   }
 })();
